84c939457f59f304f0f73c96e52380a4df53491b
tuoersiben <nieben201@hotmail.com>
2017-01-15 09:55:31 +0800

    问题修正

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\View;
 
 class ArticleController extends Controller
 {
